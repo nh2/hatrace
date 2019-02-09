fe579f0c3d0adcaec1a28c2be68e1cac6d0cc5c1
Niklas Hambüchen <mail@nh2.me>
2019-02-09 20:37:34 +0100

    Implement printing write syscall details

--- src/HaTrace.hs
+++ src/HaTrace.hs
@@ -89,10 +89,18 @@ traceForkExecvFullPath args = do
           Right stopType -> do
             case stopType of
               SyscallStop SyscallEnter -> do
-                syscall <- getEnteredSyscall childPid
-                putStrLn $ "Entering syscall: " ++ show syscall
+                (syscall, syscallArgs) <- getEnteredSyscall childPid
+                details <- case syscall of
+                  KnownSyscall Syscall_write -> do
+                    let SyscallArgs{ arg0 = fd, arg1 = bufAddr, arg2 = bufLen } = syscallArgs
+                    let bufPtr = wordPtrToPtr (fromIntegral bufAddr)
+                    writeBs <- peekBytes (TracedProcess childPid) bufPtr (fromIntegral bufLen)
+                    return $ "write(" ++ show fd ++ ", " ++ show writeBs ++ ")"
+                  _ -> return ""
+                putStrLn $ "Entering syscall: " ++ show syscall ++ (if details /= "" then " " ++ details else "")
               SyscallStop SyscallExit -> do
-                putStrLn $ "Exited syscall"
+                result <- getExitedSyscallResult childPid
+                putStrLn $ "Exited syscall; result: " ++ show result
               SignalDeliveryStop sig -> do
                 printSignal sig
             loop newState
@@ -224,6 +232,14 @@ data Syscall
   deriving (Show, Eq)
 
 
+data SyscallArgs = SyscallArgs
+  { arg0 :: !Word64
+  , arg1 :: !Word64
+  , arg2 :: !Word64
+  , arg3 :: !Word64
+  , arg4 :: !Word64
+  , arg5 :: !Word64
+  } deriving (Eq, Ord, Show)
 
 
 -- A good resource for syscall numbers across all architectures is
@@ -246,15 +262,25 @@ syscallNumberToName_x64_64 number =
 -- | Returns the syscall that we just entered after `waitForSyscall`.
 --
 -- PRE:
--- This must be called /only/ after `waitForSyscall`; otherwise it may throw
--- an `error` when trying to decode opcodes.
-getEnteredSyscall :: CPid -> IO (Syscall)
+-- This must be called /only/ after `waitForSyscall` made us /enter/ a sycall;
+-- otherwise it may throw an `error` when trying to decode opcodes.
+getEnteredSyscall :: CPid -> IO (Syscall, SyscallArgs)
 getEnteredSyscall cpid = do
   regs <- ptrace_getregs cpid
   case regs of
-    X86 X86Regs{ orig_eax } -> do
-      pure $ syscallNumberToName_i386 orig_eax
-    X86_64 X86_64Regs{ orig_rax, rip } -> do
+    X86 regs_i386@X86Regs{ orig_eax } -> do
+      let syscall = syscallNumberToName_i386 orig_eax
+      let args =
+            SyscallArgs
+              { arg0 = fromIntegral $ ebx regs_i386
+              , arg1 = fromIntegral $ ecx regs_i386
+              , arg2 = fromIntegral $ edx regs_i386
+              , arg3 = fromIntegral $ esi regs_i386
+              , arg4 = fromIntegral $ edi regs_i386
+              , arg5 = fromIntegral $ ebp regs_i386
+              }
+      pure (syscall, args)
+    X86_64 regs_x86_64@X86_64Regs{ orig_rax, rip } -> do
       -- Check whether it's an x86_64 or a legacy i386 syscall,
       -- and look up syscall number accordingly.
 
@@ -279,9 +305,30 @@ getEnteredSyscall cpid = do
       -- be removed from the Kernel soon:
       -- https://lkml.org/lkml/2018/12/10/1151
 
-      pure $ if is_i386_mode
-        then syscallNumberToName_i386 (fromIntegral orig_rax)
-        else syscallNumberToName_x64_64 orig_rax
+      let (syscallNumber, args) = if
+            | is_i386_mode ->
+                ( syscallNumberToName_i386 (fromIntegral orig_rax)
+                , SyscallArgs
+                    { arg0 = fromIntegral $ rbx regs_x86_64
+                    , arg1 = fromIntegral $ rcx regs_x86_64
+                    , arg2 = fromIntegral $ rdx regs_x86_64
+                    , arg3 = fromIntegral $ rsi regs_x86_64
+                    , arg4 = fromIntegral $ rdi regs_x86_64
+                    , arg5 = fromIntegral $ rbp regs_x86_64
+                    }
+                )
+            | otherwise ->
+                ( syscallNumberToName_x64_64 orig_rax
+                , SyscallArgs
+                    { arg0 = rdi regs_x86_64
+                    , arg1 = rsi regs_x86_64
+                    , arg2 = rdx regs_x86_64
+                    , arg3 = r10 regs_x86_64
+                    , arg4 = r8 regs_x86_64
+                    , arg5 = r9 regs_x86_64
+                    }
+                )
+      pure (syscallNumber, args)
 
 -- The opcode detection method idea above was motivated by:
 --
@@ -290,3 +337,16 @@ getEnteredSyscall cpid = do
 --   * https://superuser.com/questions/834122/how-to-distinguish-syscalls-form-int-80h-when-using-ptrace/1403397#1403397
 --   * removal: https://github.com/strace/strace/commit/1f84eefc409291354d0dc7db0866eaf27967da42#diff-3abc305048b4c1c134d1cd2e0eb7799eL113
 -- * Linus Torvalds in https://lore.kernel.org/lkml/CA+55aFzcSVmdDj9Lh_gdbz1OzHyEm6ZrGPBDAJnywm2LF_eVyg@mail.gmail.com/
+
+
+-- | Returns the result that we just exited after `waitForSyscall`.
+--
+-- PRE:
+-- This must be called /only/ after `waitForSyscall` made us /exit/ a sycall;
+-- the returned values may be memory garbage.
+getExitedSyscallResult :: CPid -> IO Word64
+getExitedSyscallResult cpid = do
+  regs <- ptrace_getregs cpid
+  pure $ case regs of
+    X86 X86Regs{ eax } -> fromIntegral eax
+    X86_64 X86_64Regs{ rax } -> rax
