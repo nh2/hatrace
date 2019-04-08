00d74dd6dc5cd95133df86b6cdf36fa521c4cf63
Kirill Zaborsky <qrilka@gmail.com>
2019-04-09 00:07:12 +0300

    Improve test program for access syscall

--- example-programs/access-itself.c
+++ example-programs/access-itself.c
@@ -1,6 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+
+void die_usage(void)
+{
+  fprintf(stderr, "Usage: access-itself\n");
+  exit(1);
+}
 
 int main(int argc, char const *argv[])
 {
-  access(argv[0], X_OK);
+  if (argc > 1) {
+    die_usage();
+  }
+  int res = access(argv[0], X_OK);
+  if (res == -1) {
+    perror("access");
+    exit(1);
+  }
 }
