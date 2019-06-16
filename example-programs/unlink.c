b0ade16e13a1385955f31409f8c8234d805dfcbd
Dominik Bollmann <dominikbollmann@gmail.com>
2019-06-16 16:06:54 +0200

    add test for the unlink() syscall

---
+++ example-programs/unlink.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+  if (argc != 2) {
+    puts("unlink /path/to/file/to/delete\n");
+    return 1;
+  }
+
+  const char* filepath = argv[1];
+  if (access(filepath, 0) != 0) {
+    printf("couldn't access filepath %s", filepath);
+    return 2;
+  }
+
+  int retval = unlink(filepath);
+  if (retval != 0) {
+    printf("couldn't unlink file %s", filepath);
+  }
+
+  return retval;
+}
