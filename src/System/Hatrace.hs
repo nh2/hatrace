cc27bbff31e45ba7315497aae55d518770c91b0e
Kirill Zaborsky <qrilka@gmail.com>
2019-06-26 00:17:57 +0300

    Use proper syscall name in output

--- src/System/Hatrace.hs
+++ src/System/Hatrace.hs
@@ -924,7 +924,7 @@ data SyscallEnterDetails_brk = SyscallEnterDetails_brk
 
 instance SyscallEnterFormatting SyscallEnterDetails_brk where
   syscallEnterToFormatted SyscallEnterDetails_brk{ addr } =
-    FormattedSyscall "symlink" [formatArg addr]
+    FormattedSyscall "brk" [formatArg addr]
 
 
 data SyscallExitDetails_brk = SyscallExitDetails_brk
