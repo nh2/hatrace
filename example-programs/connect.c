f90c455cbce2375b8a4690f8f438d66146842100
Stefan Jaax <stefan.jaax@googlemail.com>
2019-06-26 23:57:28 +0200

    Add listener to unix socket

--- example-programs/connect.c
+++ example-programs/connect.c
@@ -7,14 +7,16 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <errno.h>
 
 void die_usage(void){
     fprintf(stderr, "Usage: connect [FLAG]\n");
     exit(1);
 }
 
-void error_connect(void){
-    printf("\n Error: Could not connect to socket.");
+void error_connect(const char* str){
+    int err = errno;
+    printf("\n Error: Could not connect to %s socket: %s \n", str, strerror(err));
     exit(1);
 }
 
@@ -29,7 +31,7 @@ void connectInet6(){
     addr.sin6_port = htons(5000);
     inet_pton(AF_INET6, "::1", &addr.sin6_addr);
     if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0){
-        error_connect();
+        error_connect("inet6");
     }
     close(sockfd);
 }
@@ -44,7 +46,7 @@ void connectInet(){
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0){
-        error_connect();
+        error_connect("inet");
     }
     close(sockfd);
 }
@@ -59,9 +61,34 @@ void connectNetlink(){
     addr.nl_family = AF_NETLINK;
     addr.nl_pid = getpid(); /* self pid */
     if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0){
-        error_connect();
+        error_connect("netlink");
     }
     close(sockfd);
+}
+
+int listenUnixSocket(){
+    int sockfd = socket(PF_UNIX, SOCK_STREAM, 0);
+    if(sockfd < 0){
+        printf("\n Error: Could not create unix socket \n");
+        exit(1);
+    }
+    struct sockaddr_un addr;
+    memset(&addr, 0, sizeof(struct sockaddr_un));
+    addr.sun_family = AF_UNIX;
+    snprintf(addr.sun_path, UNIX_PATH_MAX, "./test_socket");
+    if(unlink(addr.sun_path) != 0 && errno != ENOENT){
+        printf("\n Failed to unlink.");
+        exit(1);
+    }
+    if(bind(sockfd, (struct sockaddr*) &addr, sizeof(addr)) != 0){
+        printf("\n Failed to bind to Unix socket.");
+        exit(1);
+    }
+    if(listen(sockfd, 5) != 0){
+        printf("\n Failed to listen to socket file descriptor.");
+        exit(1);
+    }
+    return sockfd;
 }
 
 void connectUnix(){
@@ -73,9 +100,14 @@ void connectUnix(){
     struct sockaddr_un addr;
     memset(&addr, 0, sizeof(struct sockaddr_un));
     addr.sun_family = AF_UNIX;
-    snprintf(addr.sun_path, UNIX_PATH_MAX, "./demo_socket");
-    if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0){
-        error_connect();
+    snprintf(addr.sun_path, UNIX_PATH_MAX, "./test_socket");
+    int listenfd = listenUnixSocket();
+    if(connect(sockfd, (struct sockaddr *) &addr, sizeof(addr)) != 0){
+        error_connect("unix");
+    }
+    if(close(listenfd) != 0 || close(sockfd) != 0){
+        printf("\n Failed to close Unix socket.");
+        exit(1);
     }
 }
 
