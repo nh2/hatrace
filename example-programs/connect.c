a6f175768819f8be846e3da8b1c78d98e0d3d569
Stefan Jaax <stefan.jaax@googlemail.com>
2019-06-26 22:09:42 +0200

    Reformat connect.c and add error checks for connect sys call

--- example-programs/connect.c
+++ example-programs/connect.c
@@ -8,14 +8,17 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-void die_usage(void)
-{
+void die_usage(void){
  	fprintf(stderr, "Usage: connect [FLAG]\n");
  	exit(1);
 }
 
-void connectInet6()
-{
+void error_connect(void){
+    printf("\n Error: Could not connect to socket.");
+    exit(1);
+}
+
+void connectInet6(){
 	int sockfd = socket(AF_INET6, SOCK_STREAM, 0);
  	if(sockfd < 0){
         printf("\n Error : Could not create Inet6 socket \n");
@@ -25,13 +28,14 @@ void connectInet6()
     addr.sin6_family = AF_INET6;
     addr.sin6_port = htons(5000);
     inet_pton(AF_INET6, "::1", &addr.sin6_addr);
-    connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0){
+        error_connect();
+    }
     close(sockfd);
 }
 
 
-void connectInet()
-{
+void connectInet(){
  	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
  	if(sockfd < 0){
         printf("\n Error : Could not create Inet socket \n");
@@ -39,12 +43,13 @@ void connectInet()
     }
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+    if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0){
+        error_connect();
+    }
     close(sockfd);
 }
 
-void connectNetlink()
-{
+void connectNetlink(){
 	int sockfd = socket(PF_NETLINK, SOCK_RAW, 0);
 	if(sockfd < 0){
 		printf("\n Error: Could not create netlink socket \n");
@@ -53,12 +58,13 @@ void connectNetlink()
 	struct sockaddr_nl addr;
 	addr.nl_family = AF_NETLINK;
     addr.nl_pid = getpid(); /* self pid */
-	connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+	if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0){
+        error_connect();
+    }
 	close(sockfd);
 }
 
-void connectUnix()
-{
+void connectUnix(){
 	int sockfd = socket(PF_UNIX, SOCK_STREAM, 0);
 	if(sockfd < 0){
 		printf("\n Error: Could not create unix socket \n");
@@ -68,16 +74,16 @@ void connectUnix()
 	memset(&addr, 0, sizeof(struct sockaddr_un));
 	addr.sun_family = AF_UNIX;
 	snprintf(addr.sun_path, UNIX_PATH_MAX, "./demo_socket");
- 	connect(sockfd, (struct sockaddr *)&addr, sizeof(addr));
+ 	if(connect(sockfd, (struct sockaddr *)&addr, sizeof(addr)) != 0){
+        error_connect();
+    }
 }
 
-void connectPacket()
-{
+void connectPacket(){
 	/* TODO: implement connect call for packet socket */
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
 	if (argc != 2){
 		die_usage();
 	}
