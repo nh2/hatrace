45c5d502b71bdc340095a91f7008d700a21277e5
Kirill Zaborsky <qrilka@gmail.com>
2019-07-17 00:25:38 +0300

    Fix whitespace issues

--- example-programs/sysinfo-loads.c
+++ example-programs/sysinfo-loads.c
@@ -21,12 +21,12 @@ int main(int argc, char const *argv[])
   }
 
   float f_load = 1.f / (1 << SI_LOAD_SHIFT);
-  
+
   printf("load avg 1 minute: %.2f%% CPU\n"
-	 "load avg 5 minutes: %.2f%% CPU\n"
-	 "load avg 15 minuites: %.2f%% CPU\n",
-	 info.loads[0] * f_load * 100,
-	 info.loads[1] * f_load * 100,
-	 info.loads[2] * f_load * 100);
+         "load avg 5 minutes: %.2f%% CPU\n"
+         "load avg 15 minuites: %.2f%% CPU\n",
+         info.loads[0] * f_load * 100,
+         info.loads[1] * f_load * 100,
+         info.loads[2] * f_load * 100);
   return 0;
 }
