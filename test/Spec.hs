eb3d0db23f01486026aa7f41c35926f138426b03
Niklas Hambüchen <mail@nh2.me>
2019-11-24 16:56:16 +0100

    WIP: hspec-jenkins

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,15 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+ import           Test.Hspec.Formatters.Jenkins (xmlFormatter)
+ import           Test.Hspec.Runner
+
+
+ main :: IO ()
+ main = do
+   summary <- withFile "results.xml" WriteMode $ \h -> do
+      let config = defaultConfig
+            { configFormatter = xmlFormatter
+            , configHandle = h
+            }
+      hspecWith config spec
+
+   unless (summaryFailures summary == 0) $
+     exitFailure
