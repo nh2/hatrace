49e813e712f20b04cb26b3e0da11db3b598a3b31
Tristan Cacqueray <tdecacqu@redhat.com>
2020-06-25 22:32:02 +0000

    Improve dup3 test program

--- example-programs/dup3.c
+++ example-programs/dup3.c
@@ -1,8 +1,21 @@
 #define _GNU_SOURCE
 #include <fcntl.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
 
-int main() {
-	dup3(0, 1, O_CLOEXEC);
-	dup3(0, 1, 0);
+int main(int argc, char const *argv[])
+{
+  int fd;
+
+  fd = dup3(0, 1, O_CLOEXEC);
+  if (fd < 0) {
+    perror("dup3 O_CLOEXEC failed");
+    return fd;
+  }
+  fd = dup3(0, 1, 0);
+  if (fd < 0) {
+    perror("dup3 failed");
+    return fd;
+  }
 }
